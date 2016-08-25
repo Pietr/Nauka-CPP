857a0c0171963244a2eee24f85c9ba7e439cd9b8
Pietr <snajperkrs@gmail.com>
2016-08-25 13:31:52 +0200

    Skrypt bankomatu

--- Bankomat/main.cpp
+++ Bankomat/main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
 
 using namespace std;
+string PIN,pPIN;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    //Poprawny PIN
+    pPIN = "1923";
+
+    //Funkcja sprawdzaj¹ca poprawnoœæ pinu
+    cout << "Witaj!" << endl << "Wpisz swoj PIN by wyplacic pieniadze: ";
+    cin >> PIN;
+    if (PIN == pPIN)
+    {
+        cout << "PIN jest poprawny!" << endl;
+    }else{
+        cout << "PIN nie jest poprawny!" << endl;
+    }
     return 0;
 }
